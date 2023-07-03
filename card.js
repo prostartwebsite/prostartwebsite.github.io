b2dd1480228db435c674a21fe13837d15d19b188
prostartwebsite <137485705+prostartwebsite@users.noreply.github.com>
2023-07-03 14:56:08 +0300

    Delete card.js

--- card.js
+++
@@ -1,5 +0,0 @@
-const createProductButton = document.querySelector(".header__items__menu__item__create")
-
-createProductButton.addEventListener("click", function(){
-    window.location.href = '/create.html';
-})
